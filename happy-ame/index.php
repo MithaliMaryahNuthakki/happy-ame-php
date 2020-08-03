d75f9035994741e15339c1cd0113449a478b784f
MithaliMaryahNuthakki <69067041+MithaliMaryahNuthakki@users.noreply.github.com>
2020-08-03 21:39:34 +0530

    Add files via upload

---
+++ happy-ame/index.php
@@ -0,0 +1,188 @@
+<?php
+
+?>
+<html lang="en">
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="logo.png" type="image/png" sizes="16x16">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Happy âme</title>
+	<link rel="stylesheet" type="text/css" href="quotescss.css">
+  </head>
+  <body>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+	  
+	  <a class="navbar-brand" href="index.php">Happy âme</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+		  <li class="nav-item active">
+			<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+		  </li>
+		  
+		  <li class="nav-item dropdown">
+			<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			  Categories
+			</a>
+			<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+			  <a class="dropdown-item" href="selflove.html">Self Love</a>
+			  <a class="dropdown-item" href="motivation.html">Motiovational</a>
+			  <a class="dropdown-item" href="positivity.html">Positivity</a>
+			</div>
+		  </li>
+		</ul>
+		<!-- AddToAny BEGIN -->
+<div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+<a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+<a class="a2a_button_whatsapp"></a>
+</div>
+<script async src="https://static.addtoany.com/menu/page.js"></script>
+<!-- AddToAny END -->
+	  </div>
+	</nav>
+	
+	<div class="jumbotron ">
+	
+		<div class=" logo">
+		<img src="logo.PNG" height="150" width="150";>		
+		</div>
+		<h1 class="display-4">Welcome! </h1>
+		<h4 >This is a simple page dedictaed to make your days slightly better with a collection of quotes that I personally connect to the most.</h5>
+		<hr class="my-4">
+		<h2 class="lead"> The thing about quotes is that, they are short and crisp yet, they manage to have a larger impact than a million words put together.</h3>
+		
+	</div>
+	
+	
+	
+	
+		<div class="card-deck conatiner">
+		  <div class="card">
+			<img src="pictures/selflove1.jpg" class="card-img-top" alt="...">
+			<div class="card-body">
+			  <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M7.655 14.916L8 14.25l.345.666a.752.752 0 01-.69 0zm0 0L8 14.25l.345.666.002-.001.006-.003.018-.01a7.643 7.643 0 00.31-.17 22.08 22.08 0 003.433-2.414C13.956 10.731 16 8.35 16 5.5 16 2.836 13.914 1 11.75 1 10.203 1 8.847 1.802 8 3.02 7.153 1.802 5.797 1 4.25 1 2.086 1 0 2.836 0 5.5c0 2.85 2.045 5.231 3.885 6.818a22.075 22.075 0 003.744 2.584l.018.01.006.003h.002z"></path></svg> 
+									Self love</h5>
+			  <p class="card-text">It is one of the most essential trait one must posses.
+			   All of us love, cherish and put others before our self, 
+			   in this process we stop loving ourselves and transform ourself so
+			   that others love us. This is the wost kind harm you are inflicting on your self.</p>
+			   <p class="card-text selfloveadditional">WE MUST ALL PUT OURSELVES BEFORE ANYONE ELSE.</p>
+				<a href="selflove.html" class="btn btn-primary button1">Click for Quotes!</a>
+			</div>
+		  </div>
+		  <div class="card">
+			<img src="pictures/motivation.jpg" class="card-img-top" alt="...">
+			<div class="card-body">
+			  <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25z"></path></svg> Motivational</h5>
+			  <p class="card-text">All of us at some point or the other just give up,
+									mostly because we are either tired or dont care about it anymore,
+									all we need to do is take a breather and think of the wonderful things that we would achieve and what we thought about when we initially started the task.</p>
+			  <p class="card-text motivationadditional">YOU CAN MAKE IT BIG, JUST KEEP PUSHING.</p>					
+			  <a href="motivation.html" class="btn btn-primary button1">Click for Quotes!</a>
+			</div>
+		  </div>
+		  <div class="card">
+			<img src="pictures/positivity.jpg" class="card-img-top" alt="...">
+			<div class="card-body">
+			  <h5 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"></path></svg> Positivity</h5>
+			  <p class="card-text">Today everyone strives to be a perfection.
+									with thi goal to achieve they don't mind putting others down so that the could look "perfect"
+									perfection is a relative term, enjoy who you are and love others for who they really are. Spread love and joy, not hatred.</p>
+			  <p class="card-text positivityadditional">REMIND PEOPLE THEY ARE PERFECT JUST THE WAY THEY ARE.</p>					
+			  <a href="positivity.html" class="btn btn-primary button1">Click for Quotes!</a>
+			</div>
+		  </div>
+		  
+		</div>
+		
+		
+			
+<footer class="page-footer font-small indigo">
+
+  <!-- Footer Links -->
+  <div class="container">
+
+    <!-- Grid row-->
+    <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="index.php">Home</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="selflove.html">Self Love</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="motivation.html">Motivation</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="positivity.html">Positivity</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      </div>
+    <!-- Grid row-->
+    <hr class="rgba-white-light" style="margin: 0 15%;">
+
+    <!-- Grid row-->
+    <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+
+      <!-- Grid column -->
+      <div class="col-md-8 col-12 mt-5">
+        
+         <img class="logofooter" src="logo.PNG" height="50" width="50"></p>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+    <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+    
+  </div>
+ 
+  <div class="footer-copyright text-center py-3">
+	© 2020 Copyright:Mithali Maryah Nuthakki
+	<h6 class="text-uppercase font-weight-bold">
+          <a href="https://www.linkedin.com/in/nuthakki-mithali-maryah-b254a1194">LinkedIn</a>
+        </h6>
+	
+  </div>
+  <!-- Copyright -->
+
+</footer>
+		
+		
+		
+
+  
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
